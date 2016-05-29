af63ef26229532b596ec541aee8e13e2532eec5f
Manoel Vilela <manoel_vilela@engineer.com>
2016-05-29 14:51:04 -0300

    Rename preprocessing->processing and put the prototype functions

---
+++ mreport/processing.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+#   Python Script
+#
+#   Copyright © Manoel Vilela
+#
+#
+
+import pandas as pd
+
+
+def malloc(csv):
+    pass
+
+
+def free(csv):
+    pass
+
+
+def diff(df):
+    pass
+
+
+def average(dfs):
+    pass
