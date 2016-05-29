e237852aeed4489c192ac0de71baed1df6613415
Manoel Vilela <manoel_vilela@engineer.com>
2016-05-29 14:54:30 -0300

    Add the initial template of the command line interface
    We'll have these args:
    show & save graph with matplotlib:
        "--show-graph"
        '--save-graph'
    
    control input/output:
        "csvs" -> all the args parsed without flags will be here
        '-t', '--target' -> the output path
    
    processing control:
    	=> to handle the long-size
        '-l', '--long-size', (float in seconds)
        "-s", "--slice-size", (int)

--- mreport/cli.py
+++ mreport/cli.py
@@ -6,3 +6,73 @@
 #   Copyright © Manoel Vilela
 #
 #
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+# active the graph print
+parser.add_argument(
+    "--show-graph",
+    dest='show_graph',
+    default=False,
+    action='store_true',
+    help="Show the graph after pre-processing"
+)
+# save the graph on the end
+parser.add_argument(
+    '--save-graph',
+    dest='save_graph',
+    default=False,
+    action='store_true',
+    help="Show the "
+
+)
+
+# the target path to save the report
+parser.add_argument(
+    '-t', '--target',
+    dest='target',
+    default='memory-analysis-report',
+    help="The path (can be a folder name or path) to save the output"
+)
+
+# the long size
+parser.add_argument(
+    '-l', '--long-size',
+    dest='long',
+    default=1,
+    type=float,
+    help="The long size in seconds to labelize the allocation time"
+)
+
+# the slice to control the how long is counted
+parser.add_argument(
+    "-s", "--slice-size",
+    dest='slice',
+    default=1000,
+    type=int,
+    help="The sice of slice to count the number of longs in the rows"
+
+)
+
+# the default argument evaluation handled about the csv files
+# or directories
+parser.add_argument(
+    "csvs",
+    help="The list of pair malloc_0x.csv free_0x separated by spaces or a dir",
+    nargs='+'
+)
+
+
+def parser_arguments(args):
+    pass
+
+
+def main():
+    options = parser.parse_args()
+    print(options)
+
+# parser.set_defaults(func=parser_arguments)
+
+if __name__ == '__main__':
+    main()
