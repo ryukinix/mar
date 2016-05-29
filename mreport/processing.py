a5aee51d5da0e3fd63e535179addd8a8f8a62356
Manoel Vilela <manoel_vilela@engineer.com>
2016-05-29 16:16:35 -0300

    Implement on processing: parse, diff, labelize and average
    We need yet implement the function 'stats', whose count the 'slice' as
    'longs'.

--- mreport/processing.py
+++ mreport/processing.py
@@ -10,21 +10,31 @@
 import pandas as pd
 
 
-def read_malloc(csv):
-    return pd.read_csv(csv)
+def parse(csvs):
+    mallocs = sorted(x for x in csvs if 'malloc' in x)
+    frees = sorted(x for x in csvs if 'free' in x)
+    dfs = zip(mallocs, frees)
+    return [(pd.read_csv(m), pd.read_csv(f)) for m, f in dfs]
 
 
-def read_free(csv):
-    return pd.read_csv(csv)
+def diff(malloc, free, by='time', index='memory_id',
+         m_columns=('req', 'op', 'memory_id'),
+         f_columns=('op', 'memory_id')):
+    malloc.sort_values(index, inplace=True)
+    free.sort_values(index, inplace=True)
+    df_diff = pd.merge(malloc, free)
+    df_diff[by] = (malloc[by] - free[by])
+    return df_diff
 
 
-def calculate_diff(malloc_df, free_df):
-    pass
+def labelize(df, limit, label='long'):
+    df[label] = df.time.map(lambda x: x > limit / 1000)
+    return df
 
 
-def average(dfs):
-    pass
+def average(dfs, by='time'):
+    return pd.merge([x[by] for x in dfs]).mean(axis=1)[by]
 
 
-def stats(csvs):
+def stats(dfs, slice):
     pass
