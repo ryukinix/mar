25e004fe26de0e3aef4983d7f7da0a19a8ae5e5a
Manoel Vilela <manoel_vilela@engineer.com>
2016-06-08 07:47:20 -0300

    Implementation of closed-interval done
    We need threat later a proper implementation of parsing getting [] ()
    syntax and doing the real mathematics stuff

--- mar/interval.py
+++ mar/interval.py
@@ -21,21 +21,33 @@ class Interval(object):
         self.interval = interval
         self.left, self.right = self.parse(self.interval)
 
+    def __repr__(self):
+        return '<{cls} {interval} at {id}>'.format(cls=self.__name__(),
+                                                   interval=self.interval,
+                                                   id=hex(id(self)))
+
+    def __str__(self):
+        return self.interval
+
     def parse(self, interval):
         return list(map(float, 
                         interval.strip('[]()').split(',')))
 
     def __contains__(self, element):
-        return self > element and self < element
+        return self >= element and self <= element
 
     def __gt__(self, element):
-        return element > self.right
+        return self.right > element 
 
     def __lt__(self, element):
-        return element < self.left
+        return self.left < element
 
     def __ge__(self, element):
-        return element <= self.right
+        return self.right >= element 
 
     def __le__(self, element):
-        return element >= self.right
+        return self.left <= element 
+
+    @classmethod
+    def __name__(cls):
+        return cls.__name__
